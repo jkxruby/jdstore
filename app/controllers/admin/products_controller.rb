4974bcb189f6971e74da777bc6b0cf80c9d4b5b6
jkxruby <jkxruby@gmail.com>
2017-06-29 14:56:21 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -45,7 +45,7 @@ layout "admin"
 
   private
   def product_params
-    params.require(:product).permit(:title, :description, :price, :quantity)
+    params.require(:product).permit(:title, :description, :price, :quantity, :image)
   end
 
 
