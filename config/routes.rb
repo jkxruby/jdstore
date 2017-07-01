e3095b028eba7a995115d63fb0915577f2aa4e18
jkxruby <jkxruby@gmail.com>
2017-07-01 10:30:26 +0800

    touch app/views/carts/checkkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,12 @@ end
 resources :carts do
   collection do
     delete :clean
-    post :checkout 
+    post :checkout
   end
 end
 
 resources :cart_items
+resources :orders 
 
 
 root 'products#index'
